80a6d73541cfc8dcd08c21737481eb07b84b5076
joonspoon <joonspoon@joonspoon.com>
2019-03-03 21:45:57 -0800

    Made the robot draw a square using a for loop.

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -1,15 +1,18 @@
 package examples;
 
-import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class IntroToRobot {
 	public static void main(String[] args) {
-		
-		// START HERE
 
-	
-		
-		
+		Robot r2d2 = new Robot();
+		r2d2.penDown();
+		r2d2.setPenWidth(20);
+		r2d2.setSpeed(5);
+		for (int i = 0; i < 4; i++) {
+			r2d2.move(100);
+			r2d2.turn(90);
+		}
+
 	}
 }

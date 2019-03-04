e3f42079f508de737d8d49f4b2f9884aa1f8e15a
joonspoon <joonspoon@joonspoon.com>
2019-03-03 19:12:04 -0800

    Examples and exercises related to loops.

---
+++ src/examples/IntroToRobot.java
@@ -0,0 +1,15 @@
+package examples;
+
+import java.awt.Color;
+import org.jointheleague.graphical.robot.Robot;
+
+public class IntroToRobot {
+	public static void main(String[] args) {
+		
+		// START HERE
+
+	
+		
+		
+	}
+}
---
+++ src/optional/BeerOnTheWall.java
@@ -0,0 +1,13 @@
+package optional;
+
+/* 
+ * Print the lyrics to the song "99 Bottles of Beer on the wall"
+ * 
+ * http://99-bottles-of-beer.net/lyrics.html
+ */
+
+public class BeerOnTheWall {
+	public static void main(String[] args) {
+
+	}
+}
